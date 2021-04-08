57973d0cd17513cf978161f49c363eeb015493b5
Juliapp <aragaopintojuli@gmail.com>
2021-04-08 00:28:47 -0300

    terminal controller

--- HackerChat/client/src/terminalController.js
+++ HackerChat/client/src/terminalController.js
@@ -1,7 +1,67 @@
+import ComponentBuilder from './components.js';
 export default class TerminalController {
+  #usersColors = new Map();
+
   constructor() {}
 
+  #pickColor() {
+    return `#${(((1 << 24) * Math.random()) | 0).toString(16)}-fg`;
+  }
+
+  #getUserColors(userName) {
+    if (this.#usersColors.has(userName)) return this.#usersColors.get(userName);
+
+    const color = this.#pickColor();
+    this.#usersColors.set(userName, color);
+    return color;
+  }
+
+  #onInputReceived(eventEmitter) {
+    return function () {
+      const message = this.getValue();
+      console.log(message);
+      this.clearValue();
+    };
+  }
+
+  #onMessageReceived({ screen, chat }) {
+    return (msg) => {
+      const { userName, message } = msg;
+      const color = this.#getUserColors(userName);
+      chat.addItem(`{${color}}{bold}${userName}{/}: ${message}`);
+      screen.render();
+    };
+  }
+
+  #registerEvents(eventEmitter, components) {
+    eventEmitter.on('message:received', this.#onMessageReceived(components));
+  }
+
   async initializeTable(eventEmitter) {
-    console.log('inicializou');
+    const components = new ComponentBuilder()
+      .setScreen({ title: 'HackerChat - Juh' })
+      .setLayoutComponent()
+      .setInputComponent(this.#onInputReceived(eventEmitter))
+      .setChatComponent()
+      .build();
+
+    this.#registerEvents(eventEmitter, components);
+    components.input.focus();
+    components.screen.render();
+
+    setInterval(() => {
+      eventEmitter.emit('message:received', {
+        message: 'hey',
+        userName: 'juli',
+      });
+      eventEmitter.emit('message:received', {
+        message: 'iae',
+        userName: 'bruno',
+      });
+      eventEmitter.emit('message:received', {
+        message: 'how',
+        userName: 'jsus',
+      });
+    });
   }
 }
