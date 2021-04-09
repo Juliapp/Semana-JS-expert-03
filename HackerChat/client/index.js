51c94f9c59cc851aba99e972a9fef2c854c0c3f0
Juliapp <aragaopintojuli@gmail.com>
2021-04-09 17:57:33 -0300

    cli config

--- HackerChat/client/index.js
+++ HackerChat/client/index.js
@@ -1,5 +1,12 @@
 import Events from 'events';
 import TerminalController from './src/terminalController.js';
+import CliConfig from './src/cliConfig.js';
+
+console.log(process.argv);
+
+const [, , ...commands] = process.argv;
+const config = CliConfig.parseArguments(commands);
+console.log('config', config);
 
 const componentEmitter = new Events();
 const controller = new TerminalController();
