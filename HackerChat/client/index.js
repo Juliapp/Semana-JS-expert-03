8b88c931d7ec6379195f0c16f817b3e86810efa1
Juliapp <aragaopintojuli@gmail.com>
2021-04-09 23:25:02 -0300

    socket client side

--- HackerChat/client/index.js
+++ HackerChat/client/index.js
@@ -1,14 +1,16 @@
 import Events from 'events';
 import TerminalController from './src/terminalController.js';
 import CliConfig from './src/cliConfig.js';
+import SocketClient from './src/socket.js';
 
 console.log(process.argv);
 
 const [, , ...commands] = process.argv;
 const config = CliConfig.parseArguments(commands);
-console.log('config', config);
 
 const componentEmitter = new Events();
-const controller = new TerminalController();
+const socketClient = new SocketClient(config);
+socketClient.initialize();
 
-await controller.initializeTable(componentEmitter);
+// const controller = new TerminalController();
+// await controller.initializeTable(componentEmitter);
