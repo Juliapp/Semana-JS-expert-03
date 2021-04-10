ecff90b3d191753025154ad6a8af31422e5a5eda
Juliapp <aragaopintojuli@gmail.com>
2021-04-09 23:24:48 -0300

    server socket controller

--- HackerChat/server/src/index.js
+++ HackerChat/server/src/index.js
@@ -1,43 +1,50 @@
 import SocketServer from './socket.js';
 import Event from 'events';
 import { constants } from './constants.js';
+import Controller from './controller.js';
 const eventEmitter = new Event();
 
-async function testServer() {
-  const options = {
-    port: 9898,
-    host: 'localhost',
-    headers: {
-      Connection: 'Upgrade',
-      Upgrade: 'websocket',
-    },
-  };
+// async function testServer() {
+//   const options = {
+//     port: 9898,
+//     host: 'localhost',
+//     headers: {
+//       Connection: 'Upgrade',
+//       Upgrade: 'websocket',
+//     },
+//   };
 
-  const http = await import('http');
-  const req = http.request(options);
-  req.end();
-  req.on('upgrade', (res, socket) => {
-    socket.on('data', (data) => {
-      console.log('client received', data.toString());
-    });
+//   const http = await import('http');
+//   const req = http.request(options);
+//   req.end();
+//   req.on('upgrade', (res, socket) => {
+//     socket.on('data', (data) => {
+//       console.log('client received', data.toString());
+//     });
 
-    setInterval(() => {
-      socket.write('Hello!');
-    }, 500);
-  });
-}
+//     setInterval(() => {
+//       socket.write('Hello!');
+//     }, 500);
+//   });
+// }
 
 const port = process.env.PORT || 9898;
 const socketServer = new SocketServer({ port });
 const server = await socketServer.initialize(eventEmitter);
+console.log('socket server is running at', socketServer.port);
+const controller = new Controller({ socketServer });
 
-eventEmitter.on(constants.event.NEW_USER_CONNECTED, (socket) => {
-  console.log('New connection', socket.id);
+eventEmitter.on(
+  constants.event.NEW_USER_CONNECTED,
+  controller.onNewConnection.bind(controller)
+);
 
-  socket.on('data', (data) => {
-    console.log('Server received', data.toString());
-    socket.write('World');
-  });
-});
+//   console.log('New connection', socket.id);
 
-await testServer();
+//   socket.on('data', (data) => {
+//     console.log('Server received', data.toString());
+//     socket.write('World');
+//   });
+// });
+
+// await testServer();
