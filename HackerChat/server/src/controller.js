03b48963468cdb720d8e566d9cf363a352655646
Juliapp <aragaopintojuli@gmail.com>
2021-04-11 19:43:33 -0300

    broadcast

--- HackerChat/server/src/controller.js
+++ HackerChat/server/src/controller.js
@@ -37,6 +37,27 @@ export default class Controller {
       constants.event.UPDATE_USERS,
       currentUsers
     );
+
+    this.broadCast({
+      socketId,
+      roomId,
+      message: { id: socketId, userName: userData.userName, currentUsers },
+      event: constants.event.NEW_USER_CONNECTED,
+    });
+  }
+
+  broadCast({
+    socketId,
+    roomId,
+    event,
+    message,
+    includeCurrentSocket = false,
+  }) {
+    const usersOnRoom = this.#rooms.get(roomId);
+    for (const [key, user] of usersOnRoom) {
+      if (!includeCurrentSocket && key === socketId) continue;
+      this.socketServer.sendMessage(user.socket, event, message);
+    }
   }
 
   #joinUserOnRoom(roomId, user) {
