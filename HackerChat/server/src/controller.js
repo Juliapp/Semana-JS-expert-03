2ee5c5ac383e3791635bdfd6dc309684772a33df
Juliapp <aragaopintojuli@gmail.com>
2021-04-11 16:44:14 -0300

    :sparkles: creating rooms

--- HackerChat/server/src/controller.js
+++ HackerChat/server/src/controller.js
@@ -1,5 +1,9 @@
+import { constants } from './constants.js';
+
 export default class Controller {
   #users = new Map();
+  #rooms = new Map();
+
   constructor({ socketServer }) {
     this.socketServer = socketServer;
   }
@@ -15,14 +19,47 @@ export default class Controller {
     socket.on('end', this.#onSocketCosed(id));
   }
 
-  #onSocketData(id) {
-    return (data) => {
-      console.log('onSocketData', data.toString());
-    };
+  async JoinRoom({ socketId, data }) {
+    const userData = data;
+    console.log(`${userData.userName} joined!`, [socketId]);
+
+    const { roomId } = userData;
+    const user = this.#updateGlobalUserData(socketId, userData);
+    const users = this.#joinUserOnRoom(roomId, user);
+
+    const currentUsers = Array.from(userData.values()).map(
+      ({ id, userName }) => ({
+        userName,
+        id,
+      })
+    );
+
+    this.socketServer.sendMessage(
+      user.socket,
+      constants.event.UPDATE_USERS,
+      currentUsers
+    );
+  }
+
+  #joinUserOnRoom(roomId, user) {
+    const usersOnRoom = this.#rooms.get(roomId) ?? new Map();
+    usersOnRoom.set(user.id, user);
+    this.#rooms.set(roomId, usersOnRoom);
+    return usersOnRoom;
   }
   #onSocketCosed(id) {
     return (data) => {
-      console.log('onSocketData', data.toString());
+      console.log('onSocketClosed', id);
+    };
+  }
+  #onSocketData(id) {
+    return (data) => {
+      try {
+        const { event, message } = JSON.parse(data);
+        this[event](id, message);
+      } catch (error) {
+        console.log(`wrong event format`, data.toString());
+      }
     };
   }
 
